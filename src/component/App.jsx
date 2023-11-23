2529b85268a19826fa2b3d41d96dd0b925fbe855
박지원 <jackgg12322@gmail.com>
2023-11-23 15:47:51 +0900

    feat : Delete Button 기능 구현

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,18 +1,23 @@
 import FoodList from './FoodList';
 import { useState } from 'react';
-import items from '../mock.json';
+import mockItems from '../mock.json';
 
 function App() {
+  const [items, setItems] = useState(mockItems); 
   const [order , setOrder] = useState('createdAt');
   const sortedItems = items.sort((a,b) =>b[order] - a[order]);
 
   const handleNewestClick = () => setOrder('createdAt');
   const handleCaloriesClick = () => setOrder('calorie');
+  const handleDelete =(id) => {
+    const nextItems = items.filter((item)=> item.id !== id);
+    setItems(nextItems);
+  }
     return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCaloriesClick}>칼로리순</button>
-      <FoodList items={sortedItems} />
+      <FoodList items={sortedItems} onDelete={handleDelete}/>
     </div>
   );
 }
