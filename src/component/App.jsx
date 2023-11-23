67ebb6d74db6628312f6a01ab857c355a0c96442
박지원 <jackgg12322@gmail.com>
2023-11-23 19:15:53 +0900

    feat : pagination 활용

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,35 +1,55 @@
-import { getFoods } from '../api';
-import FoodList from './FoodList';
-import { useEffect, useState } from 'react';
-
+import { useEffect, useState } from "react";
+import { getFoods } from "../api";
+import FoodList from "./FoodList";
 
 function App() {
-  const [items, setItems] = useState([]); 
-  const [order , setOrder] = useState('createdAt');
-  const sortedItems = items.sort((a,b) =>b[order] - a[order]);
+  const [order, setOrder] = useState("createdAt");
+  const [cursor, setCursor] = useState(null);
+  const [items, setItems] = useState([]);
 
-  const handleNewestClick = () => setOrder('createdAt');
-  const handleCaloriesClick = () => setOrder('calorie');
-  const handleDelete =(id) => {
-    const nextItems = items.filter((item)=> item.id !== id);
+  const handleNewestClick = () => setOrder("createdAt");
+
+  const handleCalorieClick = () => setOrder("calorie");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
 
-  const handleLoad =  async (orderQuery) =>{
-   const {foods} = await getFoods(orderQuery);
-   setItems(foods);
-  }
+  const handleLoad = async (options) => {
+    const {
+      foods,
+      paging: { nextCursor },
+    } = await getFoods(options);
+    if (!options.cursor) {
+      setItems(foods);
+    } else {
+      setItems((prevItems) => [...prevItems, ...foods]);
+    }
+    setCursor(nextCursor);
+  };
 
-  useEffect(()=>{
-    handleLoad(order);
-  },[order]);
+  const handleLoadMore = () => {
+    handleLoad({
+      order,
+      cursor,
+    });
+  };
 
-    return (
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+
+  useEffect(() => {
+    handleLoad({
+      order,
+    });
+  }, [order]);
+
+  return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
-      <button onClick={handleCaloriesClick}>칼로리순</button>
-      <FoodList items={sortedItems} onDelete={handleDelete}/>
-
+      <button onClick={handleCalorieClick}>칼로리순</button>
+      <FoodList items={sortedItems} onDelete={handleDelete} />
+      {cursor && <button onClick={handleLoadMore}>더보기</button>}
     </div>
   );
 }
