2126e6f6efddc553341878216a9c5824f7f5b7a4
박지원 <jackgg12322@gmail.com>
2023-11-23 16:41:21 +0900

    refactor : mock.json 삭제 및 네트워크 데이터 호출 Button 기능 구현

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,9 +1,10 @@
+import { getFoods } from '../api';
 import FoodList from './FoodList';
 import { useState } from 'react';
-import mockItems from '../mock.json';
+
 
 function App() {
-  const [items, setItems] = useState(mockItems); 
+  const [items, setItems] = useState([]); 
   const [order , setOrder] = useState('createdAt');
   const sortedItems = items.sort((a,b) =>b[order] - a[order]);
 
@@ -12,12 +13,18 @@ function App() {
   const handleDelete =(id) => {
     const nextItems = items.filter((item)=> item.id !== id);
     setItems(nextItems);
+  };
+
+  const handleLoadClick =  async () =>{
+   const {foods} = await getFoods();
+   setItems(foods);
   }
     return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
       <button onClick={handleCaloriesClick}>칼로리순</button>
       <FoodList items={sortedItems} onDelete={handleDelete}/>
+      <button onClick={handleLoadClick}>불러오기</button>
     </div>
   );
 }
