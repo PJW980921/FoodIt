ce86d82cf61034134e9104f1420f0ad8a0487068
박지원 <jackgg12322@gmail.com>
2023-11-23 18:15:31 +0900

    feat : 데이터 정렬

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -15,15 +15,15 @@ function App() {
     setItems(nextItems);
   };
 
-  const handleLoad =  async () =>{
-   const {foods} = await getFoods();
+  const handleLoad =  async (orderQuery) =>{
+   const {foods} = await getFoods(orderQuery);
    setItems(foods);
   }
 
   useEffect(()=>{
-    handleLoad();
-  },[]);
-  
+    handleLoad(order);
+  },[order]);
+
     return (
     <div>
       <button onClick={handleNewestClick}>최신순</button>
