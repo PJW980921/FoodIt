faf9c5c14879dc4829e161e9e4a7376f77d3fb52
박지원 <jackgg12322@gmail.com>
2023-11-24 18:49:22 +0900

    feat : ref DOM NODE 호출

--- src/component/FileInput.jsx
+++ src/component/FileInput.jsx
@@ -1,8 +1,11 @@
+import { useRef } from "react";
+
 export default function FileInput({ name, value, onChange }) {
+  const inputRef = useRef();
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
-  return <input type="file" onChange={handleChange} />;
+  return <input type="file" onChange={handleChange}  ref={inputRef}/>;
 }
