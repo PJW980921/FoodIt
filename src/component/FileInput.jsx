8f6144931e6fb4b5ad8d441ffe077c952c894d34
박지원 <jackgg12322@gmail.com>
2023-11-24 19:01:25 +0900

    feat : File Clear 기능 구현

--- src/component/FileInput.jsx
+++ src/component/FileInput.jsx
@@ -2,10 +2,24 @@ import { useRef } from "react";
 
 export default function FileInput({ name, value, onChange }) {
   const inputRef = useRef();
+
   const handleChange = (e) => {
     const nextValue = e.target.files[0];
     onChange(name, nextValue);
   };
 
-  return <input type="file" onChange={handleChange}  ref={inputRef}/>;
-}
+  const handleClearClick = () => {
+   const inputNode = inputRef.current;
+   if (!inputNode) return; 
+
+   inputNode.value = '';
+   onChange(name , null);
+  }
+
+  
+  return (
+  <div>
+  <input type="file" onChange={handleChange}  ref={inputRef}/>;
+  {value && <button onClick={handleClearClick}> X </button>}
+  </div>
+)}
