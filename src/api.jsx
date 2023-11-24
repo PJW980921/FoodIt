9f1b8e5a94a43d18baf0b24b62c4ac60b5586b2a
박지원 <jackgg12322@gmail.com>
2023-11-24 14:55:38 +0900

    refactor : 서버 변경

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,6 @@
 export async function getFoods({ order = "", cursor = "", limit = 10 }) {
   const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
-  const response = await fetch(`https://learn.codeit.kr/api/foods?${query}`);
+  const response = await fetch(`https://learn.codeit.kr/2411/foods?${query}`);
   const body = await response.json();
   return body;
 }
