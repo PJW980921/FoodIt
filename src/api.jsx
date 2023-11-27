5dcbf19586464eeb84087cb11ae277f975dbfe40
박지원 <jackgg12322@gmail.com>
2023-11-27 20:00:19 +0900

    feat : data 값 추가기능 구현

--- src/api.jsx
+++ src/api.jsx
@@ -1,6 +1,23 @@
+const BASE_URL = "https://learn.codeit.kr/2411/";
+
 export async function getFoods({ order = "", cursor = "", limit = 10 }) {
   const query = `order=${order}&cursor=${cursor}&limit=${limit}`;
-  const response = await fetch(`https://learn.codeit.kr/2411/foods?${query}`);
+  const response = await fetch(`${BASE_URL}foods?${query}`);
+  if (!response.ok) {
+    throw new Error("데이터를 불러오는데 실패했습니다.");
+  }
+  const body = await response.json();
+  return body;
+}
+
+export async function createFoods(formData) {
+  const response = await fetch(`${BASE_URL}foods?`, {
+    method: "POST",
+    body: formData,
+  });
+  if (!response.ok) {
+    throw new Error("데이터를 생성하는데 실패했습니다.");
+  }
   const body = await response.json();
   return body;
 }
--- src/component/FoodItForm.jsx
+++ src/component/FoodItForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import FileInput from "./FileInput";
+import { createFoods } from "../api";
 function sanitize(type, value) {
   switch (type) {
     case "number":
@@ -9,13 +10,18 @@ function sanitize(type, value) {
       return value;
   }
 }
+
+const INITIAL_VALUES = {
+  title: "",
+  calorie: 0,
+  content: "",
+  imgFile: null,
+};
+
 export default function FoodItForm() {
-  const [values, setValues] = useState({
-    title: "",
-    calorie: 0,
-    content: "",
-    imgFile: null,
-  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [submittingError, setSubmittingError] = useState(null);
+  const [values, setValues] = useState(INITIAL_VALUES);
 
   const handleChange = (name, value) => {
     setValues((prevValues) => ({
@@ -29,9 +35,24 @@ export default function FoodItForm() {
     handleChange(name, sanitize(type, value));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(values);
+    const formData = new FormData();
+    formData.append("title", values.title);
+    formData.append("calorie", values.calorie);
+    formData.append("content", values.content);
+    formData.append("imgFile", values.imgFile);
+    try {
+      setSubmittingError(null);
+      setIsSubmitting(true);
+      await createFoods(formData);
+    } catch (error) {
+      setSubmittingError(error);
+      return;
+    } finally {
+      setIsSubmitting(false);
+    }
+    setValues(INITIAL_VALUES);
   };
 
   return (
@@ -41,15 +62,26 @@ export default function FoodItForm() {
         value={values.imgFile}
         onChange={handleChange}
       />
-      <input value={values.foodName} onChange={handleInputChange} />
+      <input
+        name="title"
+        value={values.foodName}
+        onChange={handleInputChange}
+      />
       <input
         type="number"
         name="calorie"
         value={values.calorie}
         onChange={handleInputChange}
       />
-      <textarea value={values.content} onChange={handleInputChange} />
-      <button type="submit">확인</button>
+      <textarea
+        name="content"
+        value={values.content}
+        onChange={handleInputChange}
+      />
+      <button type="submit" disabled={isSubmitting}>
+        확인
+      </button>
+      {submittingError?.message && <div>{submittingError.message}</div>}
     </form>
   );
 }
